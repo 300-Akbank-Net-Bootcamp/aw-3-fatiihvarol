4fc2f56829a8fda0efa62e2df56d7b1729c3fd0f
fatiihvarol <fatihvrl38@gmail.com>
2023-12-24 16:46:58 +0300

    Entities created and connected to MsSql

--- VbApi/VbApi/Startup.cs
+++ VbApi/VbApi/Startup.cs
@@ -1,41 +1,38 @@
-using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+namespace VbApi;
 
-
-namespace VbApi
+public class Startup
 {
-    public class Startup
+    public IConfiguration Configuration;
+
+    public Startup(IConfiguration configuration)
     {
-        public IConfiguration Configuration;
+        Configuration = configuration;
+    }
 
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
+    public void ConfigureServices(IServiceCollection services)
+    {
+        string connection = Configuration.GetConnectionString("MsSqlConnection");
+        services.AddDbContext<VbDbContext>(options => options.UseSqlServer(connection));
         
-        public void ConfigureServices(IServiceCollection services)
-        {
-          services.AddControllers();
-          services.AddValidatorsFromAssemblyContaining<EmployeeValidator>(); // register validators
-          services.AddValidatorsFromAssemblyContaining<StaffValidator>(); // register validators
+        services.AddControllers();
         
-          services.AddEndpointsApiExplorer();
-          services.AddSwaggerGen();
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+    
+    public void Configure(IApplicationBuilder app,IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseRouting();
-            app.UseAuthorization();
-            app.UseEndpoints(x => { x.MapControllers(); });
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(x => { x.MapControllers(); });
     }
 }
